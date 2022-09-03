738c06a533c41bc01af569d75b52f1f33d0f9800
Mr.Gerd26 <75909419+FuntimeGerd26@users.noreply.github.com>
2022-09-02 21:47:13 -0300

    Add files via upload

---
+++ Projectiles/Ranged/XShotArrow.cs
@@ -0,0 +1,74 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GMR.Projectiles.Ranged
+{
+	public class XShotArrow : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Ultra-Blue Arrow");
+			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 10;
+			ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 60;
+			Projectile.height = 60;
+			Projectile.aiStyle = 1;
+			Projectile.penetrate = 4;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.timeLeft = 1200;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = true;
+			Projectile.extraUpdates = 1;
+			AIType = ProjectileID.Bullet;
+		}
+
+		public override void AI()
+		{
+			Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.ToRadians(45f);
+			Projectile.localAI[1] = 1f;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			Projectile.velocity = Projectile.velocity * 2;
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			Main.instance.LoadProjectile(Projectile.type);
+			Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
+			Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+			for (int k = 0; k < Projectile.oldPos.Length; k++)
+			{
+				Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY);
+				Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+				Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+			}
+
+			return true;
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+			SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+			int dustId = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 48, Projectile.velocity.X * 0.5f,
+	        	Projectile.velocity.Y * 0.2f, 60, default(Color), 2f);
+			Main.dust[dustId].noGravity = true;
+			int dustId3 = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 48, Projectile.velocity.X * 0.5f,
+				Projectile.velocity.Y * 0.2f, 60, default(Color), 2f);
+			Main.dust[dustId3].noGravity = true;
+		}
+	}
+}
