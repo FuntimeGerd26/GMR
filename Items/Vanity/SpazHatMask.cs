3f4e8757ee41fd692d4a78a2a111bfc41c1aabe0
Mr.Gerd26 <75909419+FuntimeGerd26@users.noreply.github.com>
2022-09-26 21:14:40 -0300

    Add files via upload

---
+++ Items/Vanity/SpazHatMask.cs
@@ -0,0 +1,27 @@
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GMR.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class SpazHatMask : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Spazmatanium's Hat");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 26;
+			Item.height = 34;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(silver: 30);
+			Item.vanity = true;
+			Item.maxStack = 1;
+		}
+	}
+}
---
+++ Items/Vanity/SpazThighs.cs
@@ -0,0 +1,27 @@
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GMR.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class SpazThighs : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Spazmatanium's Thighs");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 22;
+			Item.height = 16;
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(silver: 30);
+			Item.vanity = true;
+			Item.maxStack = 1;
+		}
+	}
+}
