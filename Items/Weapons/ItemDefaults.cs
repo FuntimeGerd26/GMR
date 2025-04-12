bb58001976fb987e26108cadf5b5c48fb83c3605
Mr.Gerd26 <75909419+FuntimeGerd26@users.noreply.github.com>
2025-04-12 20:43:50 -0300

    Updated Item Defaults
    the actual final file to update before fixing leftover stuff

--- Items/Weapons/ItemDefaults.cs
+++ Items/Weapons/ItemDefaults.cs
@@ -1,5 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.IO;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
@@ -8,7 +10,18 @@ using Terraria.ModLoader;
 namespace GMR.Items.Weapons
 {
 	public static class ItemDefaults
-	{
+    {
+        public static void FixSwing(this Item item, Player player)
+        {
+            if (item.pick > 0 || item.axe > 0 || item.hammer > 0)
+            {
+                if (player.toolTime > 0 && player.itemTime == 0 || !player.controlUseItem)
+                    return;
+                player.itemAnimation = Math.Min(player.itemAnimation, player.toolTime);
+            }
+            player.itemAnimation = player.itemAnimationMax;
+        }
+
         public static void DefaultToDopeSword<T>(this Item item, int swingTime) where T : ModProjectile
         {
             item.useTime = swingTime;
@@ -17,7 +30,6 @@ namespace GMR.Items.Weapons
             item.shootSpeed = 1f;
             item.DamageType = DamageClass.Melee;
             item.useStyle = ItemUseStyleID.Shoot;
-            item.autoReuse = true;
             item.channel = true;
             item.noMelee = true;
             item.noUseGraphic = true;
