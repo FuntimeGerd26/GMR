83f9bc314158b556c275304336efbb4cdc4cb879
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2025-04-15 20:26:13 -0400

    fix dupicate entities

--- Items/Vanity/SandwavePants.cs
+++ Items/Vanity/SandwavePants.cs
@@ -5,22 +5,27 @@ using Terraria.ModLoader;
 
 namespace GMR.Items.Vanity
 {
-	[AutoloadEquip(EquipType.Legs)]
-	public class SandwavePants : ModItem
-	{
-		public override void SetStaticDefaults()
-		{
-			DisplayName.SetDefault("Sandwave Pants");
-			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-		}
+    [AutoloadEquip(EquipType.Legs)]
+    public class SandwavePants : ModItem
+    {
+        public override bool IsLoadingEnabled(Mod mod)
+        {
+            return false;
+        }
 
-		public override void SetDefaults()
-		{
-			Item.width = 22;
-			Item.height = 18;
-			Item.rare = ItemRarityID.Blue;
-			Item.value = Item.sellPrice(silver: 30);
-			Item.vanity = true;
-		}
-	}
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sandwave Pants");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 22;
+            Item.height = 18;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.sellPrice(silver: 30);
+            Item.vanity = true;
+        }
+    }
 }
